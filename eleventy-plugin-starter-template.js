34c05c0351460a33ab669407af7b50b6390f1a2c
John M. Wargo <johnwargo@users.noreply.github.com>
2023-07-08 14:52:45 -0400

    it works

--- eleventy-plugin-starter-template.js
+++ eleventy-plugin-starter-template.js
@@ -1,20 +1,18 @@
 // TODO: pass two parameters to RandomNumbers
 
 const randomNumbers = (eleventyConfig) => {
-  eleventyConfig.addCollection('RandomNumbers', (collection, limit = 10) => {
-
+  eleventyConfig.addCollection('RandomNumbers', () => {
     let numbers = [];
-
-    for (let i = 0; i < limit; i++) {
-      numbers.push(Math.floor(Math.random() * 100));
+    for (let i = 0; i < 10; i++) {
+      numbers.push(Math.floor(Math.random() * 10));
     }
     return numbers;
   });
 }
 
 const sayHello = (eleventyConfig) => {
-  eleventyConfig.addFilter("sayHello", (name, tag="H2") => {
-        return `<${tag}>Hello, ${name}</${tag}>`;
+  eleventyConfig.addFilter("sayHello", (name, tag = "H2") => {
+    return `<${tag}>Hello, ${name}</${tag}>`;
   });
 };
 
@@ -22,11 +20,19 @@ const commaize = (eleventyConfig) => {
   eleventyConfig.addFilter("commaize", function (num, locale = "en-us") {
     return num.toLocaleString(locale);
   });
-
 };
+
+const hello = (eleventyConfig) => {
+  eleventyConfig.addShortcode("hello", function (firstName, lastName) {
+    return `<p>Hello, ${firstName} ${lastName}</p>`;
+  });
+};
+
+
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(commaize);
+  eleventyConfig.addPlugin(hello);
   eleventyConfig.addPlugin(randomNumbers);
   eleventyConfig.addPlugin(sayHello);
 }
