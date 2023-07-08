f1c60fad7f72e09ee98823312cb4b03196cdb79d
John M. Wargo <johnwargo@users.noreply.github.com>
2023-07-08 07:41:20 -0400

    working away

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,17 @@
-const starterTemplate = require('./src/eleventy-plugin-starter-template.js');
+const starterTemplate = require('./eleventy-plugin-starter-template.js');
 
 module.exports = eleventyConfig => {
 
   eleventyConfig.addPlugin(starterTemplate);
 
-  return {
-    dir: { output: "_site" }
-  }
+	return {
+		dir: {
+			input: 'src',
+			output: "_site",
+			includes: "_includes",
+			layouts: "_layouts",
+			data: "_data"
+		}
+	}
 
 };
